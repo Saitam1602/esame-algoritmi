cfe8e370be2d00474b8b2f55ea2383c3c4c04291
Saitam1602 <matias.maiorano@icloud.com>
2022-06-27 16:33:20 +0200

    funzioni iniziali

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,30 +1,45 @@
-# -*- coding: utf-8 -*-
-import click
-import logging
-from pathlib import Path
-from dotenv import find_dotenv, load_dotenv
+import pandas as pd
 
+def load_data(path):
+    # Pulizia generale
+    df = pd.read_csv(path)
+    df.drop(["Utilities"], axis=1)
+    df.drop(["Utilities"], axis=1)
+    df.LotFrontage = df.LotFrontage.fillna(round(df.LotFrontage.mean(),1))
+    df.Exterior1st = df.Exterior1st.dropna()
+    df.Exterior2nd = df.Exterior2nd.dropna()
+    df.MasVnrType = df.MasVnrType.dropna()
+    df.MasVnrArea = df.MasVnrArea.fillna(round(df.MasVnrArea.mean(),1))
+    df.BsmtQual = df.BsmtQual.fillna("No Basement") # nan in NA -> no basement
+    df.BsmtCond = df.BsmtCond.fillna("No Basement")
+    df.BsmtExposure = df.BsmtExposure.fillna("No Basement")
+    df.BsmtFinType1 = df.BsmtFinType1.fillna("No Basement")
+    df.BsmtFinType2 = df.BsmtFinType2.fillna("No Basement")
+    df.BsmtFinSF1 = df.BsmtFinSF1.fillna("No Basement")
+    df.BsmtFinSF2 = df.BsmtFinSF2.fillna("No Basement")
+    df.BsmtUnfSF = df.BsmtUnfSF.fillna(round(df.BsmtUnfSF.mean(),1))
+    df.TotalBsmtSF = df.TotalBsmtSF.fillna(round(df.TotalBsmtSF.mean(),1))
+    df.BsmtFullBath = df.BsmtFullBath.dropna()
+    df.BsmtHalfBath = df.BsmtHalfBath.dropna()
+    df.KitchenQual = df.KitchenQual.dropna()
+    df.Functional = df.Functional.dropna()
+    df.Fireplaces = df.Fireplaces.fillna(0)
+    df.FireplaceQu= df.FireplaceQu.fillna(0)
+    df.GarageType = df.GarageType.fillna("No garage")
+    df.GarageYrBlt = df.GarageYrBlt.fillna(df.GarageYrBlt.mean())
+    df.GarageFinish = df.GarageFinish.fillna("No garage")
+    df.GarageCars = df.GarageCars.dropna()
+    df.GarageQual = df.GarageQual.fillna("No garage")
+    df.GarageCond = df.GarageCond.fillna("No garage")
+    df.PoolQC = df.PoolQC.fillna("No pool")
+    df.Fence = df.Fence.fillna("No fence")
+    df.MiscFeature = df.MiscFeature.fillna("none")
+    df.SaleType = df.SaleType.dropna()
 
-@click.command()
-@click.argument('input_filepath', type=click.Path(exists=True))
-@click.argument('output_filepath', type=click.Path())
-def main(input_filepath, output_filepath):
-    """ Runs data processing scripts to turn raw data from (../raw) into
-        cleaned data ready to be analyzed (saved in ../processed).
-    """
-    logger = logging.getLogger(__name__)
-    logger.info('making final data set from raw data')
+    # Outlier
+    df.SalePrice.sort_values(ascending=False)
 
+    to_remove = df[df.SalePrice > 730000].index
+    df.drop(to_remove)
 
-if __name__ == '__main__':
-    log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-    logging.basicConfig(level=logging.INFO, format=log_fmt)
-
-    # not used in this stub but often useful for finding various files
-    project_dir = Path(__file__).resolve().parents[2]
-
-    # find .env automagically by walking up directories until it's found, then
-    # load up the .env entries as environment variables
-    load_dotenv(find_dotenv())
-
-    main()
+    return df
---
+++ src/features/build_features.py
@@ -0,0 +1,19 @@
+from sklearn.preprocessing import OrdinalEncoder
+
+
+def encode_categorical_data(df):
+    oe = OrdinalEncoder()
+
+    categorical_columns = df.select_dtypes(include=['object']).columns.tolist()
+
+    for c in categorical_columns:
+        df[[c]] = oe.fit_transform(df[[c]])
+
+    return df
+
+def get_best_correlation_columns(df, column, threshold=.5):
+    correlation = df.corr()
+    correlation_level = threshold
+    correlation_price = correlation[column].abs().sort_values(ascending=False)
+    correlation_columns = correlation_price[correlation_price > correlation_level].index[1:]
+    return correlation_columns
