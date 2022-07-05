9dbbeb2f3f93d355677a53c6bda5bc86eeefa688
Saitam1602 <matias.maiorano@icloud.com>
2022-07-05 17:22:15 +0200

    feature update

--- src/main.py
+++ src/main.py
@@ -7,19 +7,29 @@ from sklearn.preprocessing import OrdinalEncoder
 
 
 # Preprocessing
-path = "../data/raw/train.csv"
+print("Preprocessing")
+path = "../data/raw/test.csv"
 df = load_data(path)
 
+# print(df['MSZoning'].unique())
+print(df.isna().sum().values)
+print(df.isna().sum()[20:])
+print(df.GarageCars.unique())
+print(df.GarageCars.mode())
+exit()
 
 # Encoding
+print("Encoding")
 oe = OrdinalEncoder()
 df = multi_ordinal_encoder(df, oe)
 
 # Colonne migliori, le colonne restituite erano state calcolate nel file del training dei modelli
+print("Selezione colonne")
 best_correlation_column = get_best_column_list()
 X_test = choose_columns(df, best_correlation_column)
 
 # Caricamento modelli
+print("Caricamento Modelli")
 decision_tree_model = pickle.load(
     open('models/models/decision_tree_regressor_model.sav', 'rb'))
 random_forest_model = pickle.load(
@@ -30,18 +40,21 @@ xgboost_model = pickle.load(
     open('models/models/xgboost_regressor_model.sav', 'rb'))
 
 # Predict
+print("Predict")
 predict_decision_tree = decision_tree_model.predict(X_test)
 predict_random_forest = random_forest_model.predict(X_test)
 predict_ada_boost = ada_boost_model.predict(X_test)
 predict_xgboost = xgboost_model.predict(X_test)
 
 # Dataframe per visualizzare
+print("Creazione DataFrame")
 df_ris = pd.DataFrame()
 df_ris['predict_decision_tree'] = predict_decision_tree
 df_ris['predict_random_forest'] = predict_random_forest
 df_ris['predict_ada_boost'] = predict_ada_boost
 df_ris['predict_xgboost'] = predict_xgboost
 
-
-output = ""
+#Salvataggio risultati
+print("Salvataggio")
+output = "result.csv"
 df_ris.to_csv(output)
