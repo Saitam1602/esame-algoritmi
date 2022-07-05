de647b07b7d8e10f4fce6f33aa9b8096a2ceae4b
Saitam1602 <83394544+Saitam1602@users.noreply.github.com>
2022-07-05 21:19:41 +0200

    TODO: Comments

--- notebooks/functions/data/make_dataset.py
+++ notebooks/functions/data/make_dataset.py
@@ -3,44 +3,46 @@ import pandas as pd
 def load_data(path):
     # Pulizia generale
     df = pd.read_csv(path)
-    df.drop(["Utilities"], axis=1)
-    df.drop(["Utilities"], axis=1)
+    df = df.drop(["Utilities"], axis=1)
     df.Alley = df.Alley.fillna("No alley access")
     df.LotFrontage = df.LotFrontage.fillna(round(df.LotFrontage.mean(),1))
-    df.Exterior1st = df.Exterior1st.dropna()
-    df.Exterior2nd = df.Exterior2nd.dropna()
-    df.MasVnrType = df.MasVnrType.dropna()
+    df.Exterior1st = df.Exterior1st.fillna("Other")
+    df.Exterior2nd = df.Exterior2nd.fillna("Other")
+    df.MasVnrType = df.MasVnrType.fillna("None")
     df.MasVnrArea = df.MasVnrArea.fillna(round(df.MasVnrArea.mean(),1))
-    df.BsmtQual = df.BsmtQual.fillna("No Basement") # nan in NA -> no basement
+    df.MSZoning = df.MSZoning.fillna("No info")
+    df.BsmtQual = df.BsmtQual.fillna("No Basement") 
     df.BsmtCond = df.BsmtCond.fillna("No Basement")
     df.BsmtExposure = df.BsmtExposure.fillna("No Basement")
     df.BsmtFinType1 = df.BsmtFinType1.fillna("No Basement")
     df.BsmtFinType2 = df.BsmtFinType2.fillna("No Basement")
-    df.BsmtFinSF1 = df.BsmtFinSF1.fillna("No Basement")
-    df.BsmtFinSF2 = df.BsmtFinSF2.fillna("No Basement")
+    df.BsmtFinSF1 = df.BsmtFinSF1.fillna(round(df.BsmtFinSF1.mean(),1))
+    df.BsmtFinSF2 = df.BsmtFinSF2.fillna(round(df.BsmtFinSF2.mean(),1))
     df.BsmtUnfSF = df.BsmtUnfSF.fillna(round(df.BsmtUnfSF.mean(),1))
     df.TotalBsmtSF = df.TotalBsmtSF.fillna(round(df.TotalBsmtSF.mean(),1))
-    df.BsmtFullBath = df.BsmtFullBath.dropna()
-    df.BsmtHalfBath = df.BsmtHalfBath.dropna()
-    df.KitchenQual = df.KitchenQual.dropna()
-    df.Functional = df.Functional.dropna()
+    df.BsmtFullBath = df.BsmtFullBath.fillna(1)
+    df.BsmtHalfBath = df.BsmtHalfBath.fillna(0)
+    df.KitchenQual = df.KitchenQual.fillna("TA")
+    df.Functional = df.Functional.fillna("Typ")
     df.Fireplaces = df.Fireplaces.fillna(0)
     df.FireplaceQu= df.FireplaceQu.fillna("No Fireplace")
+    df.GarageArea = df.GarageArea.fillna(round(df.GarageArea.mean(),1))
     df.GarageType = df.GarageType.fillna("No garage")
     df.GarageYrBlt = df.GarageYrBlt.fillna(df.GarageYrBlt.mean())
     df.GarageFinish = df.GarageFinish.fillna("No garage")
-    df.GarageCars = df.GarageCars.dropna()
+    df.GarageCars = df.GarageCars.fillna(2)
     df.GarageQual = df.GarageQual.fillna("No garage")
     df.GarageCond = df.GarageCond.fillna("No garage")
     df.PoolQC = df.PoolQC.fillna("No pool")
     df.Fence = df.Fence.fillna("No fence")
     df.MiscFeature = df.MiscFeature.fillna("none")
-    df.SaleType = df.SaleType.dropna()
+    df.SaleType = df.SaleType.fillna("No info")
 
     # Outlier
-    df.SalePrice.sort_values(ascending=False)
+    if 'SalePrice' in df.columns.tolist():
+        df.SalePrice.sort_values(ascending=False)
 
-    to_remove = df[df.SalePrice > 730000].index
-    df.drop(to_remove)
+        to_remove = df[df.SalePrice > 730000].index
+        df = df.drop(to_remove)
 
     return df
--- notebooks/functions/features/build_features.py
+++ notebooks/functions/features/build_features.py
@@ -3,18 +3,32 @@ import pandas as pd
 from sklearn.preprocessing import OrdinalEncoder
 
 
-def encode_categorical_data(df):
-    oe = OrdinalEncoder()
+def multi_ordinal_encoder(df, ordinal_encoder):
 
     categorical_columns = df.select_dtypes(include=['object']).columns.tolist()
+
     for c in categorical_columns:
-        df[[c]] = oe.fit_transform(df[[c]])
-        print(df[[c]])
+        df[[c]] = ordinal_encoder.fit_transform(df[[c]])
+
     return df
+
 
 def get_best_correlation_columns(df, column, threshold=.5):
     correlation = df.corr()
     correlation_level = threshold
     correlation_price = correlation[column].abs().sort_values(ascending=False)
-    correlation_columns = correlation_price[correlation_price > correlation_level].index[1:]
+    correlation_columns = correlation_price[correlation_price >
+                                            correlation_level].index[1:]
     return correlation_columns
+
+
+def choose_columns(df, columns):
+    df_new = pd.DataFrame()
+    for c in columns:
+        df_new[[c]] = df[[c]]
+    return df_new
+
+def get_best_column_list():
+    return ['OverallQual', 'GrLivArea', 'GarageCars', 'ExterQual', 'GarageArea',
+       'TotalBsmtSF', '1stFlrSF', 'BsmtQual', 'KitchenQual', 'FullBath',
+       'TotRmsAbvGrd', 'YearBuilt', 'YearRemodAdd']
